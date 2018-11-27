942eebccec445e5fff44c06456c7f12dff569ab5
Lucas Petti <luc_petti@hotmail.com>
2018-11-27 18:07:51 +0100

    implemented the search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @buddy = User.all
+    # @buddy = User.find_by(buddy: true)
+    @user = User.new
+    @specialties = ["Anmeldung", "Bank Account", "Tax advise"]
   end
 
   def search
-    #raise
+    # raise
   end
 end
--- app/policies/buddy_policy.rb
+++ app/policies/buddy_policy.rb
@@ -1,25 +1,29 @@
 class BuddyPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show
+    true
+  end
+
+  def update?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
+  end
+
   class Scope < Scope
     def resolve
       scope.all
-    end
-
-    def index
-      record.user == user
-    end
-
-    def update?
-      record.user == user
-      # - record: the restaurant passed to the `authorize` method in controller
-      # - user:   the `current_user` signed in with Devise.
-    end
-
-    def destroy?
-      record.user == user
-    end
-
-    def create?
-      return true
     end
   end
 end
