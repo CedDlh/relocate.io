50c93af582c47e9dcdd64f13ec585e4d4ec88f89
Lucas Petti <luc_petti@hotmail.com>
2018-11-27 14:52:38 +0100

    included pg-search gem

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,11 +1,11 @@
 class BuddiesController < ApplicationController
   def index
-    # @buddies = User.where(buddy: true, id: params[:id])
+    # @buddies = policy_scope(User).where(buddy: true, params)
     @buddies = policy_scope(User).order(created_at: :desc)
   end
 
   def show
-    @buddy = User.find(buddy: true, id: params[:id])
+    @buddy = User.find(id: params[:id])
     authorize @buddy
   end
 end
