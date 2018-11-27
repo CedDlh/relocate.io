32891526bc6a6835afaeb7bd6c63cb65f7be7c84
Karl Glatzel <karl.glatzel@gmail.com>
2018-11-27 12:05:57 +0100

    added sign-up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :requests, :class_name => 'Request', :foreign_key => 'user_id'
   has_many :requests, :class_name => 'Request', :foreign_key => 'buddy_id'
 
-  # validations for both:
+  # # validations for both:
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :city, presence: true
