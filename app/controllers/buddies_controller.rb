a3e6b0c08596e4f336667a570004c5cc5dd7873d
Lucas Petti <luc_petti@hotmail.com>
2018-11-27 15:59:19 +0100

    changed buddies controller

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,6 +1,9 @@
 class BuddiesController < ApplicationController
   def index
     # @buddies = policy_scope(User).where(buddy: true, params)
+    # @search_one = Tool.search_by_location_and_category(params[:location])
+    # @search_two = Tool.search_by_location_and_category(params[:category])
+    # @buddies = @search_one & @search_two
     @buddies = policy_scope(User).search_buddy(params)
   end
 
