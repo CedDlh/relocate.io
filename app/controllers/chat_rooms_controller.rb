b5bc30caf02cf889c7dfbb9dc819a75015875d06
Lucas Petti <luc_petti@hotmail.com>
2018-11-30 15:21:23 +0100

    implemented whole next-buddy feature

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -2,6 +2,5 @@ class ChatRoomsController < ApplicationController
   def show
     @chat_room = ChatRoom.includes(messages: :user).find(params[:id])
     authorize @chat_room
-
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ApplicationRecord
-  belongs_to :user, :class_name => 'User'
-  belongs_to :buddy, :class_name => 'User'
+  belongs_to :user, class_name: 'User'
+  belongs_to :buddy, class_name: 'User'
 
   validates :status, presence: true, inclusion: { in: ["Accepted", "Declined", "Pending", "Waiting"] }
 end
