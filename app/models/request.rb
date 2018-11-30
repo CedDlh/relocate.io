3f21dc7ea505ae026f6e2fbc199b500bfbfd3c15
Lucas Petti <luc_petti@hotmail.com>
2018-11-30 12:44:49 +0100

    added the next buddy feature

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,5 @@ class Request < ApplicationRecord
   belongs_to :user, :class_name => 'User'
   belongs_to :buddy, :class_name => 'User'
 
-  validates :status, presence: true, inclusion: {in: ["Accepted", "Declined", "Pending"]}
-
+  validates :status, presence: true, inclusion: { in: ["Accepted", "Declined", "Pending", "Waiting"] }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_29_142248) do
-
+ActiveRecord::Schema.define(version: 2018_11_29_145612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +52,7 @@ ActiveRecord::Schema.define(version: 2018_11_29_142248) do
   end
 
   create_table "requests", force: :cascade do |t|
-    t.string "status", default: "Pending"
+    t.string "status"
     t.bigint "user_id"
     t.bigint "buddy_id"
     t.datetime "created_at", null: false
